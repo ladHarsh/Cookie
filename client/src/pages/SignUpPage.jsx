fc2c45efce6cb909ee001541a63a7440194e47e2
“Harsh_Lad” <ladharsh39@gmail.com>
2025-05-27 12:19:11 +0530

    Fix in signup logo.png path

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router";
-import logo from "../../public/logo.png";
 import useSignUp from "../hooks/useSignUp";
 
 const SignUpPage = () => {
@@ -81,7 +80,11 @@ const SignUpPage = () => {
         <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
           {/* LOGO */}
           <div className="mb-8 flex items-center justify-center">
-            <img src={logo} alt="Logo" className="h-16 w-auto object-contain" />
+            <img
+              src="public/logo.png"
+              alt="Logo"
+              className="h-16 w-auto object-contain"
+            />
           </div>
 
           {/* ERROR MESSAGE IF ANY */}
